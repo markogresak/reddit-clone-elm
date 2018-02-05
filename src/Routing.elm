146ca125f1e3bcb658092183d10a34822532179f
Marko Grešak <gresak.marko@gmail.com>
2018-02-05 03:32:37 +0100

    Fix location parsing after change from hash to path

--- src/Routing.elm
+++ src/Routing.elm
@@ -16,7 +16,7 @@ matchers =
 
 parseLocation : Location -> Route
 parseLocation location =
-    case (parseHash matchers location) of
+    case (parsePath matchers location) of
         Just route ->
             route
 
